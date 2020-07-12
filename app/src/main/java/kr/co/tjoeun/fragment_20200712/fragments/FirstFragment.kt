3f80426ce5a4b8b3deacc63997004e5f27a0c38e
yd27 <dhdbseh5oe@naver.com>
2020-07-12 14:45:59 +0900

    버튼 추가 + 이벤트 처리 -> 프래그먼트에서

--- app/src/main/java/kr/co/tjoeun/fragment_20200712/fragments/FirstFragment.kt
+++ app/src/main/java/kr/co/tjoeun/fragment_20200712/fragments/FirstFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
 import kr.co.tjoeun.fragment_20200712.R
 
 class FirstFragment : Fragment() {
@@ -16,6 +17,15 @@ class FirstFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
                                         //↑컨테이너에 넣어줄거야~
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        changeInfoBtn.setOnClickListener {
+            infoTxt.text = "변경된 문구"
+        }
+
     }
 
 }
