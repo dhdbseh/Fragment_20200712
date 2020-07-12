47ce91dc14e153fee8e57b950f4570118206d8f7
yd27 <dhdbseh5oe@naver.com>
2020-07-12 14:33:02 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/fragment_20200712/MainActivity.kt
@@ -0,0 +1,23 @@
+package kr.co.tjoeun.fragment_20200712
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+
+
+
+}
---
+++ app/src/main/java/kr/co/tjoeun/fragment_20200712/fragments/FirstFragment.kt
@@ -0,0 +1,4 @@
+package kr.co.tjoeun.fragment_20200712.fragments
+
+class FirstFragment {
+}
